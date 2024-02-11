b01fff1346032eb5fb6c28d06bae66be304996b7
srgamansrg <aman.srg234@gmail.com>
2024-02-12 02:36:10 +0530

    4th

---
+++ Sum_of_two_given_Numbers.py
@@ -0,0 +1,11 @@
+# Calculate sum of two given numbers
+Number1 = int(input("Enter the first number: "))
+Number2 = int(input("Enter the second number: "))
+
+print(Number1 + Number2)
+
+# Another method
+Num1 = int(input("Enter the first number: "))
+Num2 = int(input("Enter the second number: "))
+sum = Num1 + Num2
+print("The sum of the two numbers is:", sum)
