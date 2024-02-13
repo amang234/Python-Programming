bcbda97e3b632ae8ff64ee72f93fd4d77a03ed7d
srgamansrg <aman.srg234@gmail.com>
2024-02-14 00:11:53 +0530

    10th

---
+++ ForLoop.py
@@ -0,0 +1,12 @@
+# for i in range(1,11):
+#     print(i,"Hello")
+
+# for i in range(1,11,2):
+#     print(i,"Hello Brother")
+
+# for i in range(11):
+#     print(i,"Hello")
+
+list = [10,20,30,40,50,60]
+for i in list:
+    print(i)
