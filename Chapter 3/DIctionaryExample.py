509f2c876072e3c5319509204c57fc9a4eca2bdb
srgamansrg <aman.srg234@gmail.com>
2024-02-19 22:36:12 +0530

    19th

---
+++ Chapter 3/DIctionaryExample.py
@@ -0,0 +1,15 @@
+# Given a dictionary in Python, write a Python program to find the
+# sum of all items in the dictionary.
+# Input : {'a':100, 'b':200, 'c':300}
+# Output : 600
+
+Dict = {'a':100, 'b':200, 'c':300}
+print(Dict.values())
+print("Output:",sum(Dict.values()))
+
+# Input : {'x': 25, 'y':18, 'z':45}
+# Output : 88
+
+Dict = {'x': 25, 'y':18, 'z':45}
+print(Dict.values())
+print("Output:",sum(Dict.values()))
